c47916de75d6d90586857fd4d77050dfddc20e45
ruuuu <rufinka_91@mail.ru>
2021-11-20 00:14:05 +0300

    Delete src directory

--- src/index.js
+++
@@ -1,8 +0,0 @@
-import slider from './modules/slider';
-import auth from './modules/auth';  // импортиркм функцию auth из файла ./modules/auth.js
-import partners from './modules/partners';
-
-
-slider();
-auth(); // вызов фукнции
-partners(); // вызов фукнции
--- src/menu.js
+++
@@ -1,7 +0,0 @@
-import auth from './modules/auth';  // импортиркм функцию auth из файла ./modules/auth.js
-import menu from './modules/menu';
-import cart from './modules/cart';
-
-auth(); // вызов фукнции
-menu(); // вызов фукнци
-cart(); // функционал Корзина
