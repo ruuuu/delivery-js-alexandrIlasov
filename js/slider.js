291e9b738f0ae44b98962d86f8bb59e2cf9490f1
ruuuu <rufinka_91@mail.ru>
2021-11-20 00:18:09 +0300

    Add files via upload

--- js/slider.js
+++ js/slider.js
@@ -1,3 +1,40 @@
+
+import Swiper, { Autoplay, Navigation, Pagination } from 'swiper';
+
+Swiper.use([Autoplay]);
+Swiper.use([Navigation]);
+Swiper.use([Pagination]);
+
 const slider = () => {
-    
+
+    const swiper = new Swiper('.swiper', {
+
+        loop: true,
+        spaceBetween: 30,
+        centeredSlides: true,
+        autoplay: {
+            delay: 2500,
+            disableOnInteraction: false
+        },
+
+
+        pagination: {
+            el: '.swiper-pagination',
+            clickable: true,
+        },
+
+        // Navigation arrows
+        navigation: {
+            nextEl: '.swiper-button-next',
+            prevEl: '.swiper-button-prev',
+        },
+
+        // // And if we need scrollbar
+        // scrollbar: {
+        //     el: '.swiper-scrollbar',
+        // },
+    });
 }
+
+
+slider();
